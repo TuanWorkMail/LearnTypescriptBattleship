e018f046939d029cd0e11552fb7a5821627df19d
Nguyen Anh Tuan (RBVH/ESS4) <nyg1hc@bosch.com>
2016-11-11 15:35:54 +0700

    get the clicked cell position

--- main.js
+++ main.js
@@ -17,3 +17,9 @@ $(function () {
         $(this).toggleClass('destroyedCell');
     });
 });
+$('#enemyBoard').on('click', 'td', function () {
+    var $this = $(this);
+    var col = $this.index();
+    var row = $this.closest('tr').index();
+    console.log([col, row].join(','));
+});
